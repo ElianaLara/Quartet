3b91527781a50f16e4155769ce9bbd10a287fd57
amelia-77 <a.s.evans2@newcastle.ac.uk>
2026-03-07 18:37:41 +0000

    calculate_bucket added to - not finished

--- matching.py
+++ matching.py
@@ -1,8 +1,28 @@
-def calculate_match_score():
+#to calculate what bucket(s) a new user goes into
+def calculate_bucket(): #pass in the user
     pass
+    #user_score = 0
+    #for all buckets:
+        #if user.hobbies == bucket.hobbies:
+            #user_score += 3
+        #if user.music == bucket.hobbies:
+            #user_score += 3
+        #if user.education == bucket.education:
+            #user_score += 2
+        #if user.politics == bucket.education:
+            #user_score += 2
+        #if user.languages == bucket.education:
+            #user_score += 1
+        #if user.religion == bucket.religion:
+            #user_score += 1
 
+
+        #if confirm_match():
+            #write new line of table that links user to a bucket
+
+#to confirm whether a score is high enough to form a match
 def confirm_match(score):
-    if score >= 5:
+    if score >= 5: #number might change
         return True
     else:
         return False
