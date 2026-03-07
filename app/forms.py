03dd3b392467a22eb276dee62d98e8594abb1159
amelia-77 <a.s.evans2@newcastle.ac.uk>
2026-03-07 16:20:39 +0000

    initial commit

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, TextAreaField, SubmitField, IntegerField, EmailField, PasswordField, DateField, TimeField
 from wtforms.validators import DataRequired, Optional
-from wtforms_components import TimeField
 
 class LoginForm(FlaskForm):
     email = EmailField('Email', validators=[DataRequired()])
