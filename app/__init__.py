dfd6ef884a5d5ebe6831313dc7107f8938ea0e0d
ElianaLara <c3074085@newcastle.ac.uk>
2026-03-07 19:00:11 +0000

    add register

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from config import Config
 from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Global variable for db access
 db = None
@@ -10,9 +14,11 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    print(os.environ.get("MONGO_URI"))
+    print(os.environ.get("SECRET_KEY"))
     # Connect to MongoDB Atlas
     client = MongoClient(app.config['MONGO_URI'])
-    db = client.get_database()  # Uses the database in your URI
+    db = client.get_database("mydatabase")  # Uses the database in your URI
 
     # Register Blueprints
     from .routes import main
